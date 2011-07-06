fd47cec48f953dced78588127d22e4428fdff628
Thomas Maurer <thomas.maurer@simplificator.com>
2011-07-06 17:31:25 +0200

    s/xml_url/xml_settlement_url

--- lib/datatrans.rb
+++ lib/datatrans.rb
@@ -1,24 +1,18 @@
-require 'datatrans/version'
-require 'datatrans/notification'
-require 'datatrans/transaction'
-
-ActionView::Base.send :include, Datatrans::Notification::ViewHelper if defined? ActionView
-
 module Datatrans
   WEB_AUTHORIZE_URL = 'https://payment.datatrans.biz/upp/jsp/upStart.jsp'
   XML_AUTHORIZE_URL = 'https://payment.datatrans.biz/upp/jsp/XML_authorize.jsp'
-  XML_URL = 'https://payment.datatrans.biz/upp/jsp/XML_processor.jsp'
+  XML_SETTLEMENT_URL = 'https://payment.datatrans.biz/upp/jsp/XML_processor.jsp'
 
   TEST_WEB_AUTHORIZE_URL = 'https://pilot.datatrans.biz/upp/jsp/upStart.jsp'
   TEST_XML_AUTHORIZE_URL = 'https://pilot.datatrans.biz/upp/jsp/XML_authorize.jsp'
-  TEST_XML_URL = 'https://pilot.datatrans.biz/upp/jsp/XML_processor.jsp'
+  TEST_XML_SETTLEMENT_URL = 'https://pilot.datatrans.biz/upp/jsp/XML_processor.jsp'
 
   mattr_accessor :merchant_id
   mattr_accessor :sign_key
 
   mattr_reader :web_authorize_url
   mattr_reader :xml_authorize_url
-  mattr_reader :xml_url
+  mattr_reader :xml_settlement_url
   
   def self.configure
     self.environment = :development # default
@@ -28,15 +22,21 @@ module Datatrans
   def self.environment=(environment)
     case environment
     when :development
-      self.web_authorize_url = TEST_WEB_AUTHORIZE_URL
-      self.xml_authorize_url = TEST_XML_AUTHORIZE_URL
-      self.xml_url           = TEST_XML_URL
+      @@web_authorize_url  = TEST_WEB_AUTHORIZE_URL
+      @@xml_authorize_url  = TEST_XML_AUTHORIZE_URL
+      @@xml_settlement_url = TEST_XML_SETTLEMENT_URL
     when :production
-      self.web_authorize_url = WEB_AUTHORIZE_URL
-      self.xml_authorize_url = XML_AUTHORIZE_URL
-      self.xml_url           = XML_URL
+      @@web_authorize_url  = WEB_AUTHORIZE_URL
+      @@xml_authorize_url  = XML_AUTHORIZE_URL
+      @@xml_settlement_url = XML_SETTLEMENT_URL
     else
       raise "Unknown environment '#{environment}'. Available: :development, :production."
     end
   end
 end
+
+require 'datatrans/version'
+require 'datatrans/notification'
+require 'datatrans/transaction'
+
+ActionView::Base.send :include, Datatrans::Notification::ViewHelper if defined? ActionView
