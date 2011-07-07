3748fd33a5b0fe172cb6529ed9dae659d00ebc08
Thomas Maurer <thomas.maurer@simplificator.com>
2011-07-07 15:10:48 +0200

    base url added

--- lib/datatrans.rb
+++ lib/datatrans.rb
@@ -1,15 +1,18 @@
 module Datatrans
-  WEB_AUTHORIZE_URL = 'https://payment.datatrans.biz/upp/jsp/upStart.jsp'
-  XML_AUTHORIZE_URL = 'https://payment.datatrans.biz/upp/jsp/XML_authorize.jsp'
-  XML_SETTLEMENT_URL = 'https://payment.datatrans.biz/upp/jsp/XML_processor.jsp'
+  BASE_URL = 'https://payment.datatrans.biz'
+  WEB_AUTHORIZE_URL = "#{BASE_URL}/upp/jsp/upStart.jsp"
+  XML_AUTHORIZE_URL = "#{BASE_URL}/upp/jsp/XML_authorize.jsp"
+  XML_SETTLEMENT_URL = "#{BASE_URL}/upp/jsp/XML_processor.jsp"
 
-  TEST_WEB_AUTHORIZE_URL = 'https://pilot.datatrans.biz/upp/jsp/upStart.jsp'
-  TEST_XML_AUTHORIZE_URL = 'https://pilot.datatrans.biz/upp/jsp/XML_authorize.jsp'
-  TEST_XML_SETTLEMENT_URL = 'https://pilot.datatrans.biz/upp/jsp/XML_processor.jsp'
+  TEST_BASE_URL = 'https://pilot.datatrans.biz'
+  TEST_WEB_AUTHORIZE_URL = "#{TEST_BASE_URL}/upp/jsp/upStart.jsp"
+  TEST_XML_AUTHORIZE_URL = "#{TEST_BASE_URL}/upp/jsp/XML_authorize.jsp"
+  TEST_XML_SETTLEMENT_URL = "#{TEST_BASE_URL}/upp/jsp/XML_processor.jsp"
 
   mattr_accessor :merchant_id
   mattr_accessor :sign_key
 
+  mattr_reader :base_url
   mattr_reader :web_authorize_url
   mattr_reader :xml_authorize_url
   mattr_reader :xml_settlement_url
@@ -22,10 +25,12 @@ module Datatrans
   def self.environment=(environment)
     case environment
     when :development
+      @@base_url           = TEST_BASE_URL
       @@web_authorize_url  = TEST_WEB_AUTHORIZE_URL
       @@xml_authorize_url  = TEST_XML_AUTHORIZE_URL
       @@xml_settlement_url = TEST_XML_SETTLEMENT_URL
     when :production
+      @@base_url           = BASE_URL
       @@web_authorize_url  = WEB_AUTHORIZE_URL
       @@xml_authorize_url  = XML_AUTHORIZE_URL
       @@xml_settlement_url = XML_SETTLEMENT_URL
