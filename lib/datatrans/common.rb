d3efc7417599bb7297efa736a301f77cadb47954
Tobias Miesel <agileapplications@gmail.com>
2011-08-04 15:17:39 +0200

    added common method sign

---
+++ lib/datatrans/common.rb
@@ -0,0 +1,7 @@
+module Datatrans::Common
+  def sign(*fields)
+    key = Datatrans.sign_key.split(/([a-f0-9][a-f0-9])/).reject(&:empty?)
+    key = key.pack("H*" * key.size)
+    OpenSSL::HMAC.hexdigest(OpenSSL::Digest::MD5.new, key, fields.join)
+  end
+end
